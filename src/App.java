0faef1f7e66bfba6d5c7f06615bdfaaeb4c70b78
Popitas <Jorge@192.168.1.12>
2014-12-25 10:25:58 +0000

    First commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        JFrame frmPrincipal = new frmPrincipal();
+        frmPrincipal.setLocationRelativeTo(null);
+        frmPrincipal.setVisible(true);
+        
+    }
+    
+}
